529abebfbd0253f8092eef26b08ab44389c64ee4
Johnny Nguyen <94998830+johnly25@users.noreply.github.com>
2023-10-03 09:45:16 -0700

    add title to body to let users know what the app is called

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 function grid() {
     const body = document.querySelector('body');
     const container = document.createElement('div');
-    for (let column = 0; column < 16; column++) {
+    container.classList.add('container');
+
+    const title = document.createElement('h1');
+    title.textContent = "Etch-a-Sketch";
+    body.appendChild(title);
+
+        for(let column = 0; column < 16; column++) {
         const columnDiv = document.createElement('div');
         columnDiv.classList.add('column');
         const rowDiv = document.createElement('div');
         rowDiv.classList.add('row');
         for (let row = 0; row < 16; row++) {
             const div = document.createElement('div');
-            div.textContent= row;
             div.classList.add('square');
             rowDiv.appendChild(div);
-            console.log(row);
         }
         columnDiv.appendChild(rowDiv);
         container.appendChild(columnDiv);
         body.appendChild(container);
     }
 }
-
 grid();
+
+function hoverEvent(e) {
+    console.log(e);
+    e.target.classList.add('onhover');
+}
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', hoverEvent)
+});
+
+//const square = document.querySelector('.square');
+//square.addEventListener('mouseover', hoverEvent);
+
+
 
 
 
