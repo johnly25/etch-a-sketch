1a06959c529a83bdb7d429dc9fd81d56657b8255
Johnny Nguyen <94998830+johnly25@users.noreply.github.com>
2023-10-03 13:24:19 -0700

    refactor code to get rid of redundency. remove grid() function. git add . git commit -m git push origin main

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function grid() {
-    for (let column = 0; column < 16; column++) {
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add('column');
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add('row');
-        for (let row = 0; row < 16; row++) {
-            const div = document.createElement('div');
-            div.classList.add('square');
-            rowDiv.appendChild(div);
-        }
-        columnDiv.appendChild(rowDiv);
-        container.appendChild(columnDiv);
-        body.appendChild(container);
-    }
-    hoverEffect();
-}
-
 function hoverEffect() {
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
@@ -70,7 +52,7 @@ const title = document.createElement('h1');
 title.textContent = "Etch-a-Sketch";
 body.appendChild(title);
 addNewGrid();
-grid();
+generateGrid(16);
 
 
 const button = document.querySelector('.btn');
